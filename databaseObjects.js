150cf48e0b74a14eed929930b2d8f2318f2fee24
Bidhan Devkota <bidhan12@DESKTOP-0Q2RR5C.localdomain>
2022-07-10 15:35:28 -0500

    first commit

---
+++ databaseObjects.js
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize');
+
+const sequelize=new Sequelize('discordbot', 'username', 'password',{
+    host: 'localhost',
+    dialect:'sqlite',
+    logging: false,
+    storage: 'database.sqlite',
+
+});
+
+const TicTacToe = require('./models/tictactoe.js')(sequelize, Sequelize.DataTypes);
+
+module.exports={
+    TicTacToe
+
+
+};
+C:\Users\Bidhan Devkota\Desktop\disc bot\databaseObjects.js
+
---
+++ models/tictactoe.js
@@ -0,0 +1,18 @@
+
+module.exports = (sequelize,DataTypes) =>{
+return sequelize.define('tictactoe', {
+
+    user_id:{
+        type:DataTypes.STRING,
+        primaryKey:true,
+
+    },
+    score: {
+        type:DataTypes.INTEGER,
+        defaultValue:0,
+        allowNull: false,
+
+    }
+})
+
+}
